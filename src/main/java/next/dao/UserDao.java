4fbaf306ad29f20fd378a158bf3cf6d7b83b0589
yeongunheo <gjduddns@gmail.com>
2020-07-17 15:50:41 +0900

    feat: UserDao 클래스에 회원 전체 목록을 조회하는 findAll() 메소드 추가

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -41,7 +41,37 @@ public class UserDao {
 
     public List<User> findAll() throws SQLException {
         // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            
+            rs = pstmt.executeQuery();
+            List<User> userList = new ArrayList<User>();
+            while (rs.next()) {
+                User user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email"));
+                userList.add(user);
+            }
+            
+            return userList;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws SQLException {
